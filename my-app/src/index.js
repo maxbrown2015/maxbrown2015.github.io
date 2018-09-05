c881ff31f7b504509e89e24afb36fc14e9311ddd
maxbrown2015 <maxwellbrown2015@gmail.com>
2018-09-05 15:13:13 -0400

    reducing picture file size

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
